78bcb8ac9b167042749b7f1057b5d340963c6a7d
Ozan <ozancangu24@gmail.com>
2022-12-07 15:45:08 +0300

    -holidaysapi created

---
+++ app/src/main/java/com/ozancanguz/mvvmpublicholidaysinusa/data/HolidaysApi.kt
@@ -0,0 +1,12 @@
+package com.ozancanguz.mvvmpublicholidaysinusa.data
+
+import com.ozancanguz.mvvmpublicholidaysinusa.model.Holiday
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface HolidaysApi {
+
+
+    @GET("/api/v2/publicholidays/2020/US")
+    suspend fun getAllHolidays(): Response<List<Holiday>>
+}
---
+++ app/src/main/java/com/ozancanguz/mvvmpublicholidaysinusa/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.ozancanguz.mvvmpublicholidaysinusa.util
+
+class Constants {
+    companion object{
+        const val BASE_URL="https://date.nager.at/"
+    }
+}
